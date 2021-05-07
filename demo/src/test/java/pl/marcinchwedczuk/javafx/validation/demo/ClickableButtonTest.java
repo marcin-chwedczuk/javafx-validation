39a3849c4fcb22771a55d6c62d3dacb0cad22ffc
0xmarcin <0xmarcin+dev@gmail.com>
2021-05-07 19:43:52 +0200

    Add PoC of TestFX

---
+++ demo/src/test/java/pl/marcinchwedczuk/javafx/validation/demo/ClickableButtonTest.java
@@ -0,0 +1,69 @@
+package pl.marcinchwedczuk.javafx.validation.demo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
+public class ClickableButtonTest {
+
+    private Button button;
+
+    /**
+     * Will be called with {@code @Before} semantics, i. e. before each test method.
+     *
+     * @param stage - Will be injected by the test runner.
+     */
+    @Start
+    public void start(Stage stage) {
+        button = new Button("click me!");
+        button.setId("myButton");
+        button.setOnAction(actionEvent -> button.setText("clicked!"));
+        stage.setScene(new Scene(new StackPane(button), 100, 100));
+        stage.show();
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
+    @Test
+    @Disabled
+    public void should_contain_button_with_text(FxRobot robot) {
+        Assertions.assertThat(button).hasText("click me!");
+        // or (lookup by css id):
+        Assertions.assertThat(robot.lookup("#myButton").queryAs(Button.class)).hasText("click me!");
+        // or (lookup by css class):
+        Assertions.assertThat(robot.lookup(".button").queryAs(Button.class)).hasText("click me!");
+        // or (query specific type):
+        Assertions.assertThat(robot.lookup(".button").queryButton()).hasText("click me!");
+    }
+
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
+    @Test
+    @Disabled
+    public void when_button_is_clicked_text_changes(FxRobot robot) throws InterruptedException {
+        // when:
+        robot.clickOn(".button");
+
+        // then:
+        Assertions.assertThat(button).hasText("clicked!");
+        // or (lookup by css id):
+        Assertions.assertThat(robot.lookup("#myButton").queryAs(Button.class)).hasText("clicked!");
+        // or (lookup by css class):
+        Assertions.assertThat(robot.lookup(".button").queryAs(Button.class)).hasText("clicked!");
+        // or (query specific type)
+        Assertions.assertThat(robot.lookup(".button").queryButton()).hasText("clicked!");
+    }
+}
--- demo/src/test/java/pl/marcinchwedczuk/javafx/validation/demo/EmptyTestIT.java
+++
@@ -1,8 +0,0 @@
-package pl.marcinchwedczuk.javafx.validation.demo;
-
-import org.junit.jupiter.api.Test;
-
-public class EmptyTestIT {
-    @Test
-    public void empty() { }
-}
