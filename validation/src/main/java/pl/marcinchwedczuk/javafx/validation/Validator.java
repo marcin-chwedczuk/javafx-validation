fab0ded937d1ed582d46f34634cd6f81c2c6dc9d
Marcin Chwedczuk <0xmarcin+dev@gmail.com>
2021-05-22 13:16:02 +0200

    Refactor: Make return type more generic

--- validation/src/main/java/pl/marcinchwedczuk/javafx/validation/Validator.java
+++ validation/src/main/java/pl/marcinchwedczuk/javafx/validation/Validator.java
@@ -2,12 +2,13 @@ package pl.marcinchwedczuk.javafx.validation;
 
 import javafx.beans.Observable;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface Validator<T> {
     <TT extends T> ValidationResult<TT> validate(TT value);
 
-    default List<Observable> dependencies() {
+    default Collection<Observable> dependencies() {
         return List.of();
     }
 }
